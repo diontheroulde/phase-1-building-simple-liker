993b677a1d2f22c39753590e7342902b2690728b
Dion Theroulde <dionjr105@gmail.com>
2021-06-04 13:50:20 -0400

    Completed Assignment

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡"
+const FULL_HEART = "♥"
 
 // Your JavaScript code goes here!
+
+document.addEventListener("DOMContentLoaded", () => {
+  const simpleLikes = document.querySelectorAll(".like-glyph")
+  simpleLikes.forEach(simpleLike => {
+    simpleLike.addEventListener("click", function () {
+      mimicServerCall()
+        .then()
+        .catch(error => {
+          console.log(error)
+          alert("Uh-Oh!")
+          const objectError = document.querySelector("#modal")
+          objectError.classList.remove("hidden")
+          setTimeout(() => {
+            objectError.classList = "hidden"
+          }, 3000)
+        })
+      if (simpleLike.innerText === EMPTY_HEART) {
+        simpleLike.innerText = FULL_HEART
+        simpleLike.className = "activated-heart"
+      } else {
+        simpleLike.innerText = EMPTY_HEART
+        simpleLike.className = ""
+      }
+    })
+  })
+})
 
 
 
